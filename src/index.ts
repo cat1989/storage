8500d615e6254e2900d8018f404695292c91528e
cat1989 <562702740@qq.com>
2024-03-29 16:30:13 +0800

    refactor: handle parseJSON exception

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,27 @@
+const isObject = (obj: any) => Object.prototype.toString.call(obj) == "[object Object]"
+const parseJSON = (value: any) => {
+    try {
+        const obj = JSON.parse(value)
+        if (isObject(obj)) {
+            return obj
+        }
+    }
+    catch { }
+    return {}
+}
+
 export const useStorage = (namespace: string) => {
     const storage = localStorage
     const getValues = () => {
         const values = storage.getItem(namespace)
-        return values ? JSON.parse(values) as Record<string, any> : {}
+        return values ? parseJSON(values) : {}
     }
     const setValues = (values: any) => {
         storage.setItem(namespace, JSON.stringify(values))
     }
-    const get = (key: string) => {
-        const values = getValues()
-        if (Object.hasOwnProperty(key)) {
-            const {
-                value,
-                expires,
-            } = values[key]
-            if (expires === 0 || expires > new Date().getTime()) {
-                return value
-            }
-        }
-        return null
-    }
-    const set = (key: string, value: any, expires = 0) => {
-        const values = getValues()
-        values[key] = {
-            value,
-            expires,
-        }
-        setValues(values)
-    }
     const remove = (key: string) => {
         const values = getValues()
-        if (values[key]) {
+        if (values.hasOwnProperty(key)) {
             delete values[key]
         }
         setValues(values)
@@ -38,7 +29,31 @@ export const useStorage = (namespace: string) => {
     const clear = () => {
         setValues({})
     }
-    clear()
+    const get = (key: string) => {
+        const values = getValues()
+        if (values.hasOwnProperty(key)) {
+            const {
+                value,
+                expire,
+            } = values[key]
+            if (expire === 0 || expire > new Date().getTime()) {
+                return value
+            }
+            else {
+                remove(key)
+            }
+        }
+        return null
+    }
+    const set = (key: string, value: any, expire = 0) => {
+        const values = getValues()
+        values[key] = {
+            value,
+            expire,
+        }
+        setValues(values)
+    }
+    setValues(getValues())
     return Object.freeze({
         get,
         set,
